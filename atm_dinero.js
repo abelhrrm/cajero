16b580198ed3bc9630ce9a5315a26165c0df6a41
Abel Herrera <abel.herrera.montiel@outlook.com>
2021-08-15 16:41:39 -0500

    Se corrige el problema de la perdida de dinero cuando el valor a retirar no era múltiplo de 10. Se agrega un if al inicio de la función de entrega la palta, y valida si el valor es múltiplo de 10 con el operador % y se agrega la etiqueta strong para resaltar los saldos.

--- atm_dinero.js
+++ atm_dinero.js
@@ -1,9 +1,9 @@
 var  caja = [];
 var d = document.getElementById("dinero_a_retirar");
+//b = bonton "Retirar" que dispara la funcion
 var b = document.getElementById("retirar");
 var resultado = document.getElementById("resultado");
 b.addEventListener("click", entregarLaPlata)
-
 
 var cincuenta = new Billete(50, 6);
 var veinte =  new Billete(20, 10);
@@ -14,50 +14,48 @@ caja.push(diez);
 
 function entregarLaPlata()
 {
+
 	resultado.innerHTML = "";
 	var entregado = [];
 	var dinero = parseInt(d.value);
-	if (dinero < nSaldo())
-	{
-		resultado.innerHTML += ("Saldo anterior: " + nSaldo() + "<br/>");
-  	for (b of caja)
-  	{
-  		 if (dinero > 0)
-  		{
-	  			var papeles = Math.floor (dinero/b.valor);
-	  			if (papeles > b.cantidad)
-	  			{
-	  				papeles = b.cantidad;
-	  			}
-	  				entregado.push(new Billete(b.valor, papeles));
-	  				dinero = dinero - (b.valor * papeles);
-	  				b.cantidad = b.cantidad - papeles;
-  			}
-  	 }
-		 //Esta entregando dinero cuando los billetes no son multiplos de 10
-		 if (dinero == 0)
-		  {
-				 for (r of entregado)
-				 {
-					 if (r.cantidad > 0 )
-					 {
-						 resultado.innerHTML +=  "Entregado " + r.cantidad + " billetes de:  <br/>" ;
-						 resultado.appendChild(r.img);
-						 resultado.innerHTML += "<br/>"
-				   }
-				 }
-			} else
-				{
-					resultado.innerHTML = ("Por favor ingrese valores multiplos de 10 <br/>");
-				}
-
-				 	resultado.innerHTML += ("Nuevo Saldo: " + nSaldo() + "<br/>");
+		if (dinero%10 == 0)
+		{
+			if (dinero < nSaldo())
+			{
+				resultado.innerHTML += ("Saldo anterior: <strong>" + nSaldo() + "</strong><br/>");
+		  	for (b of caja)
+		  	{
+		  		 if (dinero > 0)
+		  		{
+			  			var papeles = Math.floor (dinero/b.valor);
+			  			if (papeles > b.cantidad)
+			  			{
+			  				papeles = b.cantidad;
+			  			}
+			  				entregado.push(new Billete(b.valor, papeles));
+			  				dinero = dinero - (b.valor * papeles);
+			  				b.cantidad = b.cantidad - papeles;
+		  			}
+		  	 }
+				 	for (r of entregado)
+						 {
+							 if (r.cantidad > 0 )
+							 {
+								 resultado.innerHTML +=  "Entregado " + r.cantidad + " billetes de:  <br/>" ;
+								 resultado.appendChild(r.img);
+								 resultado.innerHTML += "<br/>"
+						   }
+						 }
+					resultado.innerHTML += ("Nuevo Saldo: <strong>" + nSaldo() + "</strong><br/>");
 				} else
-					{
-					resultado.innerHTML = ("Saldo insuficiente, te falta plata");
-					}
+							{
+							resultado.innerHTML = ("Saldo insuficiente en tu cuenta");
+							}
+			} else
+						{
+								resultado.innerHTML = ("Por favor ingrese valores multiplos de 10 <br/>");
+						}
 }
-
 function nSaldo()
 {
 	var saldo = 0;
