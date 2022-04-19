29510437dcf70d413183caf19a8ffd4e1728fdd1
Abel Herrera <aherreram@outlook.com>
2022-04-19 16:26:56 -0500

    Se corrige el codigo para que pueda devolver entregar el cajero el saldo total de la cuenta, se modifica el codigo para poner el principio el valor del saldo restante antes de la entrega de los billetes. Por útlimo se agrega al etiqueta strong para al número de billetes entregados.

--- atm_dinero.js
+++ atm_dinero.js
@@ -1,4 +1,4 @@
-var  caja = [];
+var caja = [];
 var d = document.getElementById("dinero_a_retirar");
 //b = bonton "Retirar" que dispara la funcion
 var b = document.getElementById("retirar");
@@ -7,7 +7,7 @@ var valInicial = document.getElementById("valorInicial")
 b.addEventListener("click", entregarLaPlata)
 
 var cincuenta = new Billete(50, 6);
-var veinte =  new Billete(20, 10);
+var veinte = new Billete(20, 10);
 var diez = (new Billete(10, 10));
 caja.push(cincuenta);
 caja.push(veinte);
@@ -15,55 +15,42 @@ caja.push(diez);
 valInicial.innerHTML = ("El valor inicial de tu cuenta ahorro es: <strong> " + nSaldo() + "</strong><br/>");
 
 
-function entregarLaPlata()
-{
-
+function entregarLaPlata() {
 	resultado.innerHTML = "";
 	var entregado = [];
 	var dinero = parseInt(d.value);
-		if (dinero%10 == 0)
-		{
-			if (dinero < nSaldo())
-			{
-				resultado.innerHTML += ("Saldo anterior: <strong>" + nSaldo() + "</strong><br/>");
-		  	for (b of caja)
-		  	{
-		  		 if (dinero > 0)
-		  		{
-			  			var papeles = Math.floor (dinero/b.valor);
-			  			if (papeles > b.cantidad)
-			  			{
-			  				papeles = b.cantidad;
-			  			}
-			  				entregado.push(new Billete(b.valor, papeles));
-			  				dinero = dinero - (b.valor * papeles);
-			  				b.cantidad = b.cantidad - papeles;
-		  			}
-		  	 }
-				 	for (r of entregado)
-						 {
-							 if (r.cantidad > 0 )
-							 {
-								 resultado.innerHTML +=  "Entregado " + r.cantidad + " billetes de:  <br/>" ;
-								 resultado.appendChild(r.img);
-								 resultado.innerHTML += "<br/>"
-						   }
-						 }
-					resultado.innerHTML += ("Nuevo Saldo: <strong>" + nSaldo() + "</strong><br/>");
-				} else
-							{
-							resultado.innerHTML = ("Saldo insuficiente en tu cuenta");
-							}
-			} else
-						{
-								resultado.innerHTML = ("Por favor ingrese valores multiplos de 10 <br/>");
-						}
+	if (dinero % 10 == 0) {
+		if (dinero <= nSaldo()) {
+			resultado.innerHTML += ("Saldo anterior: <strong>" + nSaldo() + "</strong><br/>");
+			for (b of caja) {
+				if (dinero > 0) {
+					var papeles = Math.floor(dinero / b.valor);
+					if (papeles > b.cantidad) {
+						papeles = b.cantidad;
+					}
+					entregado.push(new Billete(b.valor, papeles));
+					dinero = dinero - (b.valor * papeles);
+					b.cantidad = b.cantidad - papeles;
+				}
+			}
+			resultado.innerHTML += ("Nuevo Saldo: <strong>" + nSaldo() + "</strong><br/>");
+			for (r of entregado) {
+				if (r.cantidad > 0) {
+					resultado.innerHTML += "Entregado <strong>" +  r.cantidad + "</strong> billetes de:  <br/>";
+					resultado.appendChild(r.img);
+					resultado.innerHTML += "<br/>"
+				}
+			}
+		} else {
+			resultado.innerHTML = ("Saldo insuficiente en tu cuenta");
+		}
+	} else {
+		resultado.innerHTML = ("Por favor ingrese valores multiplos de 10 <br/>");
+	}
 }
-function nSaldo()
-{
+function nSaldo() {
 	var saldo = 0;
-	for (b of caja)
-	{
+	for (b of caja) {
 		saldo = saldo + (b.valor * b.cantidad)
 	}
 	return saldo;
